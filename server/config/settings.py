754e87ba598a231ebe4b4e661d9468c36aad07ee
AndyAnh174 <hovietanh147@gmail.com>
2025-12-21 17:13:55 +0700

    feat: Add DRF Spectacular for API schema generation and documentation UI.

--- server/config/settings.py
+++ server/config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'rest_framework',
+    'drf_spectacular',
     'core',
 ]
 
@@ -169,3 +171,15 @@ STORAGES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'JurisQBank API',
+    'DESCRIPTION': 'API documentation for JurisQBank',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
