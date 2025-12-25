683ae05bbb8d37d46e8151bf701b9f5e04c75776
Huy Le <vogiahuy2105@gmail.com>
2025-12-25 21:20:28 +0700

    fix: useState inside useEffect in admin-layout

--- client/components/layouts/admin-layout.tsx
+++ client/components/layouts/admin-layout.tsx
@@ -1,8 +1,8 @@
 "use client"
-import { desc } from "motion/react-client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { set } from "zod";
 
 interface AdminLayoutProps {
     children: React.ReactNode;
@@ -12,36 +12,27 @@ interface AdminLayoutProps {
 export function AdminLayout({ children, sidebar  }: AdminLayoutProps) {
     const router = useRouter();
     const { data: session, status } = useSession();
-    const [isAuthorized, setIsAuthorized] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
 
     useEffect(() => {
         if (status === "loading") return;
 
-        if (!session || session.user?.role !== "ADMIN") {
-            router.push("/login"); //Redirect to login if not admin
-            return;
+        if (!session || session.user?.role !== "ADMIN") { //Redirect to login if not admin
+            router.push("/login");
         }
-
-        setIsAuthorized(true);
+        else {
+            setIsAuth(true);
+        }
     }, [session, status, router]);
 
-    if (!isAuthorized) {
-        const description = session ? "You do not have access to this page." : "Please log in to access the admin dashboard.";
-        return (
-            <div className="flex flex-col justify-center items-center min-h-screen">
-                <div className="shadow-2xl rounded-2xl border-black p-6 text-center w-fit h-fit">
-                    <h1 className="text-3xl text-red-600">404 NOT FOUND!</h1>
-                    <div className="mt-4">
-                        <p className="">{description}</p>
-                        <p>Redirecting to login..</p>
-                    </div>
-                </div>
-            </div>
-        )
+    if (!isAuth) {
+        return null
     }
 
     if (status === "loading") {
-        return <div>Loading...</div>;
+        return <div className="flex justify-center items-center min-h-screen">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>;
     }
 
     return (
