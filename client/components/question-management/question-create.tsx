5383e80adb315f7d0216f466dda5f8a0b0aea1ff
Huy Le <vogiahuy2105@gmail.com>
2026-01-13 20:29:04 +0700

    feat/question-management-ui

---
+++ client/components/question-management/question-create.tsx
@@ -0,0 +1,8 @@
+export default function QuestionCreate() {
+  return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Create New Question</h1>
+      {/* Question creation form goes here */}
+    </div>
+  );
+}
